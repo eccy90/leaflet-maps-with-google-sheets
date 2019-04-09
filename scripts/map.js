12038b3ef0eaf822011b8cca41f92d103c227395
eccy90 <eccyemmanuel@gmail.com>
2019-04-09 10:03:18 +0200

    Update map.js

--- scripts/map.js
+++ scripts/map.js
@@ -126,9 +126,12 @@ $(window).on('load', function() {
 
       if (point.Latitude !== '' && point.Longitude !== '') {
         var marker = L.marker([point.Latitude, point.Longitude], {icon: icon})
-          .bindPopup("<b>" + point['Name'] + '</b><br>' +
+          .bindPopup("<b>" +'Name of the Mine Claim'+ point['Name'] + '</b><br>' +
           (point['Image'] ? ('<img src="' + point['Image'] + '"><br>') : '') + '<b>Ward: </b>'+
-          point['Ward'] + '<br>' + '<b>Village: </b>' + point['Location']);
+                     point['Ward'] + '<br>' + '<b>Village: </b>' + point['Location'] + '<b>Operating Status:  </b>' +
+                     point['Status'] + '<b>Type of Mining:  </b>' + point['Mining_Type'] + '<b>Is this site Registered?:  </b>' 
+                     + point['Registration_Status'] + '<b>Registration Date:  </b>' + point['Registered_Date'] 
+                    + '<b>Distance from Highway(In Minutes)  :</b>' + point['Travelling_Distance'] );
 
         if (layers !== undefined && layers.length !== 1) {
           marker.addTo(layers[point.Group]);
