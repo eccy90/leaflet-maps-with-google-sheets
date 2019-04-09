aa553b803c700a80996f839879f5c57ea68ff668
eccy90 <eccyemmanuel@gmail.com>
2019-04-09 09:25:44 +0200

    Update map.js

--- scripts/map.js
+++ scripts/map.js
@@ -128,7 +128,7 @@ $(window).on('load', function() {
         var marker = L.marker([point.Latitude, point.Longitude], {icon: icon})
           .bindPopup("<b>" + point['Name'] + '</b><br>' +
           (point['Image'] ? ('<img src="' + point['Image'] + '"><br>') : '') + '<b>Ward: </b>'+
-          point['Ward']+'<br>'+point['Location']);
+          point['Ward'] + '<br>' + '<b>Village: </b>' + point['Location']);
 
         if (layers !== undefined && layers.length !== 1) {
           marker.addTo(layers[point.Group]);
